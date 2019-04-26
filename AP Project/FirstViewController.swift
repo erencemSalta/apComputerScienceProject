13eb347975ab189ce439c2b4ec2a0328677d4d41
ErenHoliday <31226698+ErenHoliday@users.noreply.github.com>
2019-04-26 11:28:06 -0400

    Initial Commit

---
+++ AP Project/FirstViewController.swift
@@ -0,0 +1,20 @@
+//
+//  FirstViewController.swift
+//  AP Project
+//
+//  Created by Eren Cem Salta on 4/26/19.
+//  Copyright © 2019 Salta Inc. All rights reserved.
+//
+
+import UIKit
+
+class FirstViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
---
+++ AP Project/SecondViewController.swift
@@ -0,0 +1,20 @@
+//
+//  SecondViewController.swift
+//  AP Project
+//
+//  Created by Eren Cem Salta on 4/26/19.
+//  Copyright © 2019 Salta Inc. All rights reserved.
+//
+
+import UIKit
+
+class SecondViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
