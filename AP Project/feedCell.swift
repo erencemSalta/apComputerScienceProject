148dd68b46c7144d8e704ed6dd6a7aaa39ba6bfc
ErenHoliday <31226698+ErenHoliday@users.noreply.github.com>
2019-05-16 13:03:03 -0400

    first commit

---
+++ AP Project/feedCell.swift
@@ -0,0 +1,24 @@
+//
+//  feedCell.swift
+//  AP Project
+//
+//  Created by Eren Cem Salta on 4/26/19.
+//  Copyright © 2019 Salta Inc. All rights reserved.
+//
+
+import UIKit
+
+class feedCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
